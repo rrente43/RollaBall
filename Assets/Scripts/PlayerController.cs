3b0347d959a7236f2090c7f4f84b64a9bca543a6
Rubi Renteria <rubirenteria43@gmail.com>
2022-12-15 00:59:05 -0700

    completed game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    //hold reference in script
     private Rigidbody rb;
+    public float speed;
+    private int count;
+    public float jumpHeight;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
+	public int numPickups;
   
     // Start is called before the first frame update
     void Start(){
 
         rb = GetComponent<Rigidbody>();
+        count = 0;
+		SetCountText();
+		winTextObject.SetActive(false);
 
     }
     //InputValue is variable type
@@ -30,7 +40,31 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3 (movementX, 0.0f ,movementY);
         //Applying Force
         rb.AddForce (movement * speed);
+        
+        if (Input.GetKeyDown("space"))
+		{
+			Vector3 jump = new Vector3 (0.0f, jumpHeight, 0.0f);
+			rb.AddForce (jump);
+		}
 
     }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("PickUp"))
+        {   
+            other.gameObject.SetActive(false);
+            count = count + 1;
+			SetCountText();
+        }
+    }
+
+    void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString();
+		if (count >= 13) 
+		{
+			winTextObject.SetActive(true);
+		}
+	}
 
 }
